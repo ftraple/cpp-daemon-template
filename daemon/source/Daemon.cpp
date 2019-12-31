e84409beb9224ef36926ddb1083049f9fb7f24dc
Fabiano Traple <fabianotraple@gmail.com>
2019-12-31 14:03:00 -0300

    Remove commented code.

--- daemon/source/Daemon.cpp
+++ daemon/source/Daemon.cpp
@@ -2,13 +2,9 @@
 #include "Daemon.hpp"
 #include "Log.hpp"
 
-//bool Daemon::m_isRunning = false;
-//bool Daemon::m_reload = false;
-
 Daemon::Daemon() {
     m_isRunning = true;
     m_reload = false;
-    // Register the signal function to read external events
     signal(SIGINT, Daemon::signalHandler);
     signal(SIGTERM, Daemon::signalHandler);
     signal(SIGHUP, Daemon::signalHandler);
